929c976482711c0eb2bc60ce7be25ea74ea11b1d
Namhyung Kim <namhyung@gmail.com>
2017-09-28 22:23:32 -0700

    dwarf: Introduce get_dwarf_{arg,ret}spec()
    The get_dwarf_argspec() is to generate argument spec using DWARF debug
    information.  When user gives no argument spec, it should be called to
    retrieve the spec automatically (if available).
    
    The get_dwarf_retspec() does the same thing for return value.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/dwarf.h
+++ utils/dwarf.h
@@ -1,18 +1,26 @@
 #ifndef UFTRACE_DWARF_H
 #define UFTRACE_DWARF_H
 
+#include <stdint.h>
+
+struct sym;
+
 #ifdef HAVE_LIBDW
 
 #include <elfutils/libdw.h>
 
 struct debug_info {
 	Dwarf		*dw;
-	unsigned long	offset;
+	uint64_t	offset;
 };
 
 extern int setup_debug_info(const char *filename, struct debug_info *dinfo,
-			    unsigned long offset);
+			    uint64_t offset);
 extern void release_debug_info(struct debug_info *info);
+extern char * get_dwarf_argspec(struct debug_info *dinfo, char *name,
+				uint64_t addr);
+extern char * get_dwarf_retspec(struct debug_info *dinfo, char *name,
+				uint64_t addr);
 
 #else /* !HAVE_LIBDW */
 
@@ -21,12 +29,22 @@ struct debug_info {
 };
 
 static inline int setup_debug_info(const char *filename, struct debug_info *dinfo,
-				   unsigned long offset)
+				   uint64_t offset)
 {
 	return -1;
 }
 
 static inline void release_debug_info(struct debug_info *dinfo) {}
+static inline char * get_dwarf_argspec(struct debug_info *dinfo, char *name,
+				       uint64_t addr)
+{
+	return NULL;
+}
+static inline char * get_dwarf_retspec(struct debug_info *dinfo, char *name,
+				       uint64_t addr)
+{
+	return NULL;
+}
 
 #endif /* HAVE_LIBDW */
 
