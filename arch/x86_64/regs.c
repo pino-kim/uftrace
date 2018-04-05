f1eff7b0bdceeb763c800628a7bea95a95214cda
Namhyung Kim <namhyung@gmail.com>
2018-04-05 22:01:08 +0900

    arch/x86: Implement arch_register_dwarf_name()
    It translates dwarf register op number to name.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- arch/x86_64/regs.c
+++ arch/x86_64/regs.c
@@ -1,9 +1,11 @@
+#include <dwarf.h>
+
 #include "mcount-arch.h"
 #include "utils/utils.h"
 
 struct x86_reg_table {
 	const char		*name;
-	enum x86_reg_index	idx;
+	int			idx;
 } reg_table[] = {
 
 #define X86_REG(_r)  { #_r, X86_REG_##_r }
@@ -27,4 +29,34 @@ int arch_register_index(char *reg_name)
 			return reg_table[i].idx;
 	}
 	return -1;
+}
+
+struct x86_reg_table dwarf_table[] = {
+	{ "rax", DW_OP_reg0, },
+	{ "rdx", DW_OP_reg1, },
+	{ "rcx", DW_OP_reg2, },
+	{ "rbx", DW_OP_reg3, },
+	{ "rsi", DW_OP_reg4, },
+	{ "rdi", DW_OP_reg5, },
+	{ "rbp", DW_OP_reg6, },
+	{ "rsp", DW_OP_reg7, },
+	{ "r8",  DW_OP_reg8, },
+	{ "r9",  DW_OP_reg9, },
+	{ "r10", DW_OP_reg10, },
+	{ "r11", DW_OP_reg11, },
+	{ "r12", DW_OP_reg12, },
+	{ "r13", DW_OP_reg13, },
+	{ "r14", DW_OP_reg14, },
+	{ "r15", DW_OP_reg15, },
+};
+
+const char * arch_register_dwarf_name(int dwarf_reg)
+{
+	unsigned i;
+
+	for (i = 0; i < ARRAY_SIZE(dwarf_table); i++) {
+		if (dwarf_reg == dwarf_table[i].idx)
+			return dwarf_table[i].name;
+	}
+	return "invalid register";
 }
